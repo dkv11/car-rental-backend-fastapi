33ea75b5401fc342fcf11d235545f792fd95d006
dkv11 <dv19961105@gmail.com>
2026-01-11 18:04:38 +0530

    add user authentication logic

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,13 +2,19 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
 from app.model.user import User
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password 
 
 
 class UserAlreadyExistsError(Exception):
     pass
 
 class InvalidUserInputError(Exception):
+    pass
+
+class UserNotFoundError(Exception):
+    pass
+
+class InvalidCredentialsError(Exception):
     pass
 
 def register_user(db: Session, username: str, password: str) -> User:
@@ -35,5 +41,20 @@ def register_user(db: Session, username: str, password: str) -> User:
     except IntegrityError:
         db.rollback()
         raise UserAlreadyExistsError(f"User with username '{username}' already exists.")
+    
+    return user
+
+def authenticate_user(db: Session, username: str, password: str) -> User:
+    if not isinstance(username, str) or not username.strip():
+        raise InvalidUserInputError("Invalid username provided.")   
+    if not isinstance(password, str) or not password:
+        raise InvalidUserInputError("Invalid password provided.")
+    
+    user = (db.query(User).filter(User.username == username.strip()).first())
+
+    if not user:
+        raise UserNotFoundError("User not found.")
+    if not verify_password(password, user.password_hash):
+        raise InvalidCredentialsError("Invalid credentials provided.")
     
     return user
